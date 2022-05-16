0ddd92b4cd34c20f0b18ae521bf4ee27c565bead
Oziel Perez <OZLperez11@gmail.com>
2022-05-16 10:50:58 -0500

    App can now fetch location coordinates

--- app/src/main/java/dreamcraft/main/MainActivity.kt
+++ app/src/main/java/dreamcraft/main/MainActivity.kt
@@ -1,14 +1,22 @@
 package dreamcraft.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import dreamcraft.main.ui.components.AppBar
 import dreamcraft.main.ui.components.Form
 import dreamcraft.main.ui.theme.GMSignInTheme
@@ -17,16 +25,22 @@ import dreamcraft.main.viewmodels.FormViewModel
 
 class MainActivity : ComponentActivity() {
     private lateinit var title: String
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private lateinit var locationClient: FusedLocationProviderClient
     private val viewModel: FormViewModel = FormViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         title = getString(R.string.main_title)
+        permissionLauncher = registerForActivityResult(RequestPermission(), ::getLocationData)
+        locationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        viewModel.populateOfficeProperty()
+        getLocationData()
 
         setContent {
             GMSignInTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.surface) {
@@ -39,7 +53,25 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun getLocationData(ready: Boolean = false) {
+        val permission: String = Manifest.permission.ACCESS_COARSE_LOCATION
+        val isGranted: Int = if (ready) 1 else PackageManager.PERMISSION_GRANTED
+
+        // Check if location permission already granted before
+        if (ContextCompat.checkSelfPermission(this, permission) == isGranted) {
+            locationClient.lastLocation.addOnSuccessListener(::onFetchLocationSuccess)
+        } else {
+            permissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    private fun onFetchLocationSuccess(location: Location) {
+        Log.d("APP", "latitude: " + location.latitude)
+        Log.d("APP", "longitude: " + location.longitude)
+    }
+
     private fun submitFormData() {
+        viewModel.generateTimestamp()
         Log.i("APP", viewModel.formData.toString())
     }
 }
--- app/src/main/java/dreamcraft/main/viewmodels/FormViewModel.kt
+++ app/src/main/java/dreamcraft/main/viewmodels/FormViewModel.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import dreamcraft.main.models.ClientSignInEntry
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FormViewModel : ViewModel() {
     var formData by mutableStateOf(ClientSignInEntry())
@@ -17,8 +19,18 @@ class FormViewModel : ViewModel() {
     val isFullNameInvalid: Boolean get () = formData.full_name == "" && isFullNameFieldDirty
     val isVisitPurposeInvalid: Boolean get () = formData.visit_purpose == "" && isVisitPurposeFieldDirty
 
-    public fun toggleVisitPurposeOptions() {
+    fun toggleVisitPurposeOptions() {
         showVisitPurposeOptions = !showVisitPurposeOptions
         if (!showVisitPurposeOptions) isVisitPurposeFieldDirty = true
+    }
+
+    fun generateTimestamp() {
+        formData = formData.copy(
+            visit_datetime = SimpleDateFormat.getDateTimeInstance().format(Calendar.getInstance().time)
+        )
+    }
+
+    fun populateOfficeProperty() {
+
     }
 }
